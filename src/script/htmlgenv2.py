d3a64a5fd93aa03fd9e8fa600cf85564d870ea99
Matteo Trranova <80316272+terranovamt@users.noreply.github.com>
2025-09-04 14:48:33 +0200

    mode to mean

--- src/script/htmlgenv2.py
+++ src/script/htmlgenv2.py
@@ -95,7 +95,7 @@ def detect_clamps(
 
     # Get basic statistics
     values = subset["Value"]
-    mean_val = values.mode()[0]
+    mean_val = values.mean()
     std_dev = values.std()
 
     # If std is 0 or very small, no clamps can be detected reliably
